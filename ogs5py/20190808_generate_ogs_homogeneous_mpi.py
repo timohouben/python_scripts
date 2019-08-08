6fe850df05ac55c870d022527af9b54607ffbe21
houben <timo.houben@ufz.de>
2019-08-08 18:31:03 +0200

    new script to generate homogeneous ogs model runs with kf values sampled from a lognormal distributuon  bugfix6

--- ogs5py/20190808_generate_ogs_homogeneous_mpi.py
+++ ogs5py/20190808_generate_ogs_homogeneous_mpi.py
@@ -85,6 +85,8 @@ size=100
 kf_list = np.random.lognormal(mean=-10,sigma=2,size=100)
 # save plots and .txt for kf values but only on one rank
 if rank == 0:
+    if not os.path.exists(CWD + "/kf_values"):
+        os.mkdir(CWD + "/kf_values")
     import seaborn as sns
     from scipy.stats.mstats import gmean, hmean
     hist_kf = plt.hist(kf_list, bins=30)
@@ -106,10 +108,8 @@ if rank == 0:
     kf_list_file.write("list of kf values\n")
     kf_list_file.write("\n".join([str(i) for i in kf_list]))
     kf_list_file.close()
-    if not os.path.exists(CWD + "/kf_values"):
-        os.mkdir(CWD + "/kf_values")
 else:
-    time.sleep(5) 
+    time.sleep(5)
 
 # Set a start value for "overall_count" which is the index. I recommend to use
 # as much digits as you will be generating new ogs models to end up with a
